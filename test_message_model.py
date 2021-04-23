b7de9714538ddec5482370ddbc078d318953c859
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-22 18:26:31 -0700

    test basic message functionality

--- test_message_model.py
+++ test_message_model.py
@@ -3,6 +3,7 @@
 import os
 from unittest import TestCase
 from models import db, User, Message
+from datetime import datetime
 
 os.environ['DATABASE_URL'] = 'postgresql:///warbler-test'
 
@@ -20,8 +21,36 @@ class MessageModelTestCase(TestCase):
         User.query.delete()
         Message.query.delete()
     
-    def tearDown():
+    def tearDown(self):
         """Clear the session."""
         db.session.rollback()
     
+    def test_message_model(self):
+        """Does basic message model work?"""
+
+        user = User.signup(
+            username="bob_dango",
+            password="dosmespa11",
+            email="dsfkk@gmail.com",
+            image_url=None
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        message = Message(
+            text="This is some nice text",
+            user_id=user.id
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        # Do we have access to the attributes?
+        self.assertIsInstance(message.text, str)
+        self.assertIsInstance(message.user_id, int)
+        self.assertIsInstance(message.timestamp, datetime)
+
+        # Do we have access to the user?
+        user = User.query.filter(User.id == user.id).one()
+        self.assertIs(user, message.user)
     
